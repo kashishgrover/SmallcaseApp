58091d820c2427629bb44efb8388065686e09200
Kashish Grover <kashishgrover95@gmail.com>
2017-06-18 03:36:29 +0530

    Updating portfolioArray and portfolioDict onClick and onDrag

--- app/js/controller.js
+++ app/js/controller.js
@@ -4,6 +4,8 @@ angular.module('SmallcaseTask.controller', [])
 
 	$scope.stockPageSize = 8;
 	$scope.currentPage = 0;
+	$scope.portfolioArray = new Array();
+	$scope.portfolioDict = {};
 
 	$http.get('data/data.json').success(function(data) {
 		$scope.stockPrices = getArray(data.price);
@@ -14,11 +16,19 @@ angular.module('SmallcaseTask.controller', [])
 	});
 
 	$scope.onDragComplete=function(data,evt){
-       console.log("drag success, data:", data);
+       // console.log("drag success, data:", data);
+       return data
     }
     $scope.onDropComplete=function(data,evt){
-        // console.log("drop success, data:", data);
-        $scope.dragText = data;
+        console.log("drop success, data:", data);
+        $scope.portfolioDict[data] = ($scope.portfolioDict[data] || 0) + 1;
+        $scope.portfolioArray = getArray(portfolioDict);
+    }
+
+    $scope.onClick=function(data) {
+        $scope.portfolioDict[data] = ($scope.portfolioDict[data] || 0) + 1;
+        $scope.portfolioArray = getArray($scope.portfolioDict);
+    	console.log($scope.portfolioArray);
     }
 
 })
