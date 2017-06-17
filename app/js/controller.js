f8576cabc1f52b1200ca80901099708714d3488c
Kashish Grover <kashishgrover95@gmail.com>
2017-06-18 02:56:15 +0530

    Dragging works with data

--- app/js/controller.js
+++ app/js/controller.js
@@ -12,6 +12,15 @@ angular.module('SmallcaseTask.controller', [])
 		$scope.stockEPS = data.eps;
 		$scope.stockHistoricals = data.historical;
 	});
+
+	$scope.onDragComplete=function(data,evt){
+       console.log("drag success, data:", data);
+    }
+    $scope.onDropComplete=function(data,evt){
+        // console.log("drop success, data:", data);
+        $scope.dragText = data;
+    }
+
 })
 
 function getArray (obj) {
