bbf73d6904b664a105d88401e5ddfb66c459e433
Kashish Grover <kashishgrover95@gmail.com>
2017-06-18 19:30:10 +0530

    Submit button in place

--- app/js/controller.js
+++ app/js/controller.js
@@ -14,8 +14,8 @@ angular.module('SmallcaseTask.controller', [])
 		$scope.stockPrices = getArray(data.price);
 		$scope.totalStocks = $scope.stockPrices.length;
     	$scope.numberOfPages = numberOfPages($scope.stockPrices, $scope.stockPageSize);
-		$scope.stockEPS = data.eps;
-		console.log($scope.stockEPS);
+		$scope.stockEPSobj = data.eps;
+		console.log("Stock EPS",$scope.stockEPSobj);
 		$scope.stockHistoricals = data.historical;
 	});
 
@@ -59,8 +59,30 @@ angular.module('SmallcaseTask.controller', [])
     	// Weightage = (Stock Price * Shares Held)/Net Worth
     	var price = element[0].split(",")[1];
     	var shareCount = element[1];
-    	var weightage = parseFloat(Math.round(((price * shareCount) / $scope.netWorth) * 100)).toFixed(2);
-    	return weightage;
+    	var weightage = (price * shareCount) / $scope.netWorth * 100;
+    	return weightage.toFixed(2);
+    }
+
+    $scope.getPERatio = function() {
+    	// Portfolio P/E Ratio = Net Worth/Sum(Stock EPS * Shares Held)
+    	var netWorth = $scope.netWorth;
+    	var array = $scope.portfolioArray;
+
+    	if (array.length === 0) {
+    		return "Null"
+    	}
+
+    	var stockEPSobj = $scope.stockEPSobj;
+
+    	var sum = 0;
+		for (var i = 0; i < array.length; i++) {
+			var stockName = array[i][0].split(",")[0];
+			var stockEPS = stockEPSobj[stockName];
+			var stockSharesHeld = array[i][1];
+			sum += (stockEPS * stockSharesHeld);
+		}
+
+		return (netWorth/sum).toFixed(2);
     }
 })
 
@@ -93,5 +115,5 @@ function calculateNetWorth(array) {
 		var count = array[i][1];
 		netWorth += (price * count);
 	}
-	return netWorth;
+	return netWorth.toFixed(2);
 }
