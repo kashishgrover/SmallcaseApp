c34ce5e889c7729e2ccf83a8670562d68d730f25
Kashish Grover <kashishgrover95@gmail.com>
2017-06-18 18:28:46 +0530

    Some calculations and css wrappers in place

--- app/js/controller.js
+++ app/js/controller.js
@@ -6,31 +6,71 @@ angular.module('SmallcaseTask.controller', [])
 	$scope.currentPage = 0;
 	$scope.portfolioArray = new Array();
 	$scope.portfolioDict = {};
+	$scope.netWorth = 0;
+
+	var isDropping = true;
 
 	$http.get('data/data.json').success(function(data) {
 		$scope.stockPrices = getArray(data.price);
 		$scope.totalStocks = $scope.stockPrices.length;
     	$scope.numberOfPages = numberOfPages($scope.stockPrices, $scope.stockPageSize);
 		$scope.stockEPS = data.eps;
+		console.log($scope.stockEPS);
 		$scope.stockHistoricals = data.historical;
 	});
 
-	$scope.onDragComplete=function(data,evt){
-       // console.log("drag success, data:", data);
-       return data
-    }
-    $scope.onDropComplete=function(data,evt){
-        console.log("drop success, data:", data);
+    $scope.onDropComplete=function(data,evt) {
+    	isDropping = true;
+        // console.log("drop success, data:", data);
         $scope.portfolioDict[data] = ($scope.portfolioDict[data] || 0) + 1;
-        $scope.portfolioArray = getArray(portfolioDict);
+        $scope.portfolioArray = getArray($scope.portfolioDict);
+        $scope.netWorth = calculateNetWorth($scope.portfolioArray);
+    	// console.log("26: ",$scope.portfolioArray);
     }
 
     $scope.onClick=function(data) {
-        $scope.portfolioDict[data] = ($scope.portfolioDict[data] || 0) + 1;
-        $scope.portfolioArray = getArray($scope.portfolioDict);
-    	console.log($scope.portfolioArray);
+    	if (isDropping) 
+    		isDropping = false;
+    	else {
+	        $scope.portfolioDict[data] = ($scope.portfolioDict[data] || 0) + 1;
+	        $scope.portfolioArray = getArray($scope.portfolioDict);
+	        $scope.netWorth = calculateNetWorth($scope.portfolioArray);
+	    	// console.log("32: ",$scope.portfolioArray);
+    	}
     }
 
+    $scope.incrementShareCount = function(element) {
+        $scope.portfolioDict[element[0]] = ($scope.portfolioDict[element[0]] || 0) + 1;        
+        $scope.portfolioArray = getArray($scope.portfolioDict);
+        $scope.netWorth = calculateNetWorth($scope.portfolioArray);
+    	// console.log("41: ",$scope.portfolioArray);
+    }
+
+    $scope.decrementShareCount = function(element) {
+    	if($scope.portfolioDict[element[0]] == 1) {
+    		delete $scope.portfolioDict[element[0]];
+        	$scope.portfolioArray = getArray($scope.portfolioDict);
+        	$scope.netWorth = calculateNetWorth($scope.portfolioArray);
+        	// console.log("48: ",$scope.portfolioArray);
+    	}
+    	else {
+    		$scope.portfolioDict[element[0]] = ($scope.portfolioDict[element[0]] || 0) - 1;
+        	$scope.portfolioArray = getArray($scope.portfolioDict);
+        	$scope.netWorth = calculateNetWorth($scope.portfolioArray);
+        	// console.log("53: ",$scope.portfolioArray);
+    	}
+    }
+
+    $scope.getWeightage = function(element) {
+    	// Weightage = (Stock Price * Shares Held)/Net Worth
+    	var price = element[0].split(",")[1];
+    	var shareCount = element[1];
+    	// console.log("Stock Price: ", price);
+    	// console.log("Shares Held: ", element[1]);
+    	// console.log("Net Worth: ", $scope.netWorth);
+    	var weightage = parseFloat(Math.round(((price * shareCount) / $scope.netWorth) * 100)).toFixed(2);
+    	return weightage;
+    }
 })
 
 function getArray (obj) {
@@ -52,4 +92,20 @@ function countProperties(obj) {
 
 function numberOfPages(dataArray, pageSize) {
 	return Math.ceil(dataArray.length / pageSize);
+}
+
+function calculateNetWorth(array) {
+	// console.log(array);
+	var arrayLength = array.length;
+	var netWorth = 0;
+	for (var i = 0; i < arrayLength; i++) {
+		var price = array[i][0].split(",")[1];
+		var count = array[i][1];
+		// console.log("Element", array[i]);
+		// console.log("Price: ",price);
+		// console.log("Count: ",count);
+		netWorth += (price * count);
+	}
+	// console.log("NetWorth = ",netWorth)
+	return netWorth;
 }
